bf5a2438883a76b5fe54fc2a1f19150d9ece9825
chriscast12 <ccastaneda1212@gmail.com>
2026-05-14 15:26:43 -0500

    Add app component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import Dashboard from "./components/Dashboard";
+
+export default function App() {
+  return (
+    <main>
+      <h1>Northwind Financial Portal</h1>
+      <Dashboard />
+    </main>
+  );
+}
